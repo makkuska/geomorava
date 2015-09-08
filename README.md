17377a13d231463d01f7df1fbbc98e200d84bf1d
Markéta Solanská <magiess@volny.cz>
2015-09-08 11:25:50 +0200

    oprava commitu - nahrán celý web :)

---
+++ README.md
@@ -0,0 +1,3 @@
+Geomorava s.r.o. | Mapy pro obce i soukromé objekty
+=======
+    Geomorava s.r.o. Nabízíme zpracování map v tištěné i webové podobě, tvorbu pasportů
---
+++ kontakty.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="cs-cz" dir="ltr">
+  <head>
+    <?php include_once("head.html") ?>
+      
+  </head>
+  <body>
+    
+    <!--[if lt IE 9]>
+      <script src="files/js/html5shiv.js"></script>
+      <script src="files/js/respond.min.js"></script>
+    <![endif]-->
+    
+    <!-- div navigator -->
+    <?php include_once("kontakty_header.html") ?>
+      
+    <!-- div služby -->
+    <?php include_once("kontakty.html") ?>
+    
+    <!-- div kontakt -->
+    <?php include_once("footer.html") ?>
+
+  </body>
+</html>
